f9c8bd99820815c1f444d5b7fcb8eed84e729453
sakshikaushik <sakshikaushik24@gmail.com>
2016-04-28 14:22:32 +0530

    Changes in NRI Page Form

--- ogserver/src/main/resources/webroot/js/views/nri_pages/nri_page.js
+++ ogserver/src/main/resources/webroot/js/views/nri_pages/nri_page.js
@@ -9,10 +9,11 @@ define([
     'bootstrap',
     'text!/templates/nri_pages/nri_page.html',
     'cloudinary_jquery',
+    '/js/slyutil.js',
     '/js/mgfirebase.js',
     '/js/consultutil.js',
     '/js/analytics.js'
-], function($, _, Backbone, Bootstrap, nriPageTemplate, CloudinaryJquery, MGF, ConsultUtil, Analytics) {
+], function($, _, Backbone, Bootstrap, nriPageTemplate, CloudinaryJquery, SlyUtil, MGF, ConsultUtil, Analytics) {
     var NriPageVIew = Backbone.View.extend({
         el: '.page',
         ref: MGF.rootRef,
@@ -27,6 +28,14 @@ define([
         render: function() {
             var authData = this.ref.getAuth();
             MGF.getUserProfile(authData, this.renderWithUserProfCallback);
+            this.ready();
+        },
+        ready: function() {
+            if ($('#brand-frame').length > 0) {
+                var $brand_frame = $('#brand-frame');
+                var $brand_wrap = $brand_frame.parent().parent();
+                SlyUtil.create($brand_wrap, '#brand-frame', '.brand-next', '.brand-prev').init();
+            }
         },
         initialize: function() {
             Analytics.apply(Analytics.TYPE_GENERAL);
