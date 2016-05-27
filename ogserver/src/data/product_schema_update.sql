c70d437e65deb0dd4cc9c0ad54ac9efeb402da99
mehaboob <mehaboob.basha@orangegubbi.com>
2016-05-27 11:06:20 +0530

    Product Sorting Functionality

--- ogserver/src/data/product_schema_update.sql
+++ ogserver/src/data/product_schema_update.sql
@@ -2,6 +2,8 @@ Use mg;
 
 ALTER TABLE `product` ADD `popularity` int(11) DEFAULT NULL;
 
-ALTER TABLE `product` ADD `relevance` int(11)) DEFAULT NULL;
+ALTER TABLE `product` ADD `relevance` int(11) DEFAULT NULL;
+
+ALTER TABLE `product` ADD `styleSortSeq` int(11) DEFAULT NULL;
 
 ALTER TABLE `product` ADD `createDt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
