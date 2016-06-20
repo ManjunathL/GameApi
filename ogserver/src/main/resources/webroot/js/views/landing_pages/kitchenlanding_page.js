11980199601a444cfad303e57bc8da6ff4c79dfa
Mehbub <mehaboob.basha@orangegubbi.com>
2016-06-20 17:38:02 +0530

    Kitchen Landing Page

--- ogserver/src/main/resources/webroot/js/views/landing_pages/kitchenlanding_page.js
+++ ogserver/src/main/resources/webroot/js/views/landing_pages/kitchenlanding_page.js
@@ -7,43 +7,54 @@ define([
     'backbone',
     'text!/templates/landing_pages/kitchenlanding_page.html',
     'cloudinary_jquery',
-    '/js/slyutil.js',
     '/js/mgfirebase.js',
     '/js/consultutil.js',
     '/js/analytics.js'
-], function($, _, Backbone, kitchenlandingPageTemplate, CloudinaryJquery, SlyUtil, MGF, ConsultUtil, Analytics) {
+], function($, _, Backbone, kitchenlandingPageTemplate, CloudinaryJquery, MGF, ConsultUtil, Analytics) {
     var KitchenLandingPageVIew = Backbone.View.extend({
-        el: '.page',
-        ref: null,
-        renderWithUserProfCallback: function(userProfData) {
-            $(this.el).html(_.template(kitchenlandingPageTemplate)({
-                'userProfile': userProfData
-            }));
-            $.cloudinary.responsive();
-        },
-        render: function() {
-            var authData = this.ref.getAuth();
-            MGF.getUserProfile(authData, this.renderWithUserProfCallback);
-            this.ready();
-        },
-        ready: function() {
-            if ($('#lpalt-frame').length > 0) {
-                var $lpalt_frame = $('#lpalt-frame');
-                var $lpalt_wrap = $lpalt_frame.parent().parent();
-                SlyUtil.create($lpalt_wrap, '#lpalt-frame', '.lpalt-next', '.lpalt-prev').init();
-            }
-            if ($('#brand-frame').length > 0) {
-                var $brand_frame = $('#brand-frame');
-                var $brand_wrap = $brand_frame.parent().parent();
-                SlyUtil.create($brand_wrap, '#brand-frame', '.brand-next', '.brand-prev').init();
-            }
-        },
-        initialize: function() {
-            this.ref = MGF.rootRef;
-            Analytics.apply(Analytics.TYPE_GENERAL);
-            $.cloudinary.config({ cloud_name: 'mygubbi', api_key: '492523411154281'});
-            _.bindAll(this, 'renderWithUserProfCallback');
-        }
-    });
+         el: '.page',
+                ref: null,
+                renderWithUserProfCallback: function(userProfData) {
+                    $(this.el).html(_.template(kitchenlandingPageTemplate)({
+                        'userProfile': userProfData
+                    }));
+                    $.cloudinary.responsive();
+                },
+                render: function() {
+                    var authData = this.ref.getAuth();
+                    MGF.getUserProfile(authData, this.renderWithUserProfCallback);
+                },
+                initialize: function() {
+                    Analytics.apply(Analytics.TYPE_GENERAL);
+                    this.ref = MGF.rootRef;
+                    $.cloudinary.config({ cloud_name: 'mygubbi', api_key: '492523411154281'});
+                    _.bindAll(this, 'renderWithUserProfCallback');
+                },
+                submit: function(e) {
+                    if (e.isDefaultPrevented()) return;
+                    e.preventDefault();
+
+                    var name = $('#contact_full_name1').val();
+                    var email = $('#contact_email_id1').val();
+                    var phone = $('#contact_contact_num1').val();
+/*
+                    var propertyName = $('#contact_property_name1').val();
+*/
+                    var query = $('#contact_city').val();
+/*
+                    var floorplan = $("#contact_floorplan1").prop('files')[0];
+*/
+
+                    ConsultUtil.submit(name, email, phone, query, null, null, null, null);
+
+                    window.App.router.navigate('/thankyou-consult-page', {
+                        trigger: true
+                    });
+
+                },
+                events: {
+                    "submit": "submit"
+                }
+            });
     return KitchenLandingPageVIew;
 });
