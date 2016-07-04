d6fbfea4a4b9e2f1fecc539a8e78f0c02c459937
smrutiog <smruti.m@orangegubbi.com>
2016-07-04 18:17:14 +0530

    Added Leadsquared form in landing pages and home page

--- ogserver/src/main/resources/webroot/js/views/consult/consult.js
+++ ogserver/src/main/resources/webroot/js/views/consult/consult.js
@@ -28,28 +28,7 @@ define([
             this.ref = MGF.rootRef;
             $.cloudinary.config({ cloud_name: 'mygubbi', api_key: '492523411154281'});
             _.bindAll(this, 'renderWithUserProfCallback');
-        }/*,
-        submit: function(e) {
-            if (e.isDefaultPrevented()) return;
-            e.preventDefault();
-
-            var name = $('#contact_full_name1').val();
-            var email = $('#contact_email_id1').val();
-            var phone = $('#contact_contact_num1').val();
-            var propertyName = $('#contact_property_name1').val();
-            var query = $('#contact_requirement1').val();
-            var floorplan = $("#contact_floorplan1").prop('files')[0];
-
-            ConsultUtil.submit(name, email, phone, query, floorplan, propertyName, null, null);
-
-            window.App.router.navigate('/thankyou-consult-page', {
-                trigger: true
-            });
-
-        },
-        events: {
-            "submit": "submit"
-        }*/
+        }
     });
     return ConsultView;
 });
--- ogserver/src/main/resources/webroot/js/views/landing_pages/emaillanding_page.js
+++ ogserver/src/main/resources/webroot/js/views/landing_pages/emaillanding_page.js
@@ -40,46 +40,6 @@ define([
             Analytics.apply(Analytics.TYPE_GENERAL);
             $.cloudinary.config({ cloud_name: 'mygubbi', api_key: '492523411154281'});
             _.bindAll(this, 'renderWithUserProfCallback');
-        },
-        submit_form1: function(e) {
-            if (e.isDefaultPrevented()) return;
-            e.preventDefault();
-
-            var name = $('#contact_full_name1').val();
-            var email = $('#contact_email_id1').val();
-            var phone = $('#contact_contact_num1').val();
-
-            ConsultUtil.submit(name, email, phone, "", "", null, null, null);
-
-            window.App.router.navigate('/thankyou-emaillp-page', {
-                trigger: true
-            });
-
-        },
-        submit_form2: function(e) {
-            if (e.isDefaultPrevented()) return;
-            e.preventDefault();
-
-            var name = $('#contact_full_name2').val();
-            var email = $('#contact_email_id2').val();
-            var phone = $('#contact_contact_num2').val();
-            var propertyName = $('#contact_property_name2').val();
-            var query = $('#contact_requirement2').val();
-            var floorplan = $("#contact_floorplan2").prop('files')[0];
-            var projectName = $('#contact_project_name2').val();
-            var cityName = $('#contact_city_name2').val();
-
-            ConsultUtil.submit(name, email, phone, query, floorplan, propertyName, projectName, cityName);
-
-            window.App.router.navigate('/thankyou-emaillp-page', {
-                trigger: true
-            });
-
-        },
-        events: {
-            "submit #contactNwLpForm1": "submit_form1",
-            "submit #contactNwLpForm2": "submit_form2"
-
         }
     });
     return EmailLandingPageVIew;
