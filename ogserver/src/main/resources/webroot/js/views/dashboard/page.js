e880e8b5838ae6348e054f7fda41e25f712dda75
smrutiog <smruti.m@orangegubbi.com>
2016-07-12 15:58:14 +0530

    Merge branch 'develop' of /var/www/html/apidevelopment with conflicts.

--- ogserver/src/main/resources/webroot/js/views/dashboard/page.js
+++ ogserver/src/main/resources/webroot/js/views/dashboard/page.js
@@ -79,28 +79,7 @@ define([
             Analytics.apply(Analytics.TYPE_GENERAL);
             $.cloudinary.config({ cloud_name: 'mygubbi', api_key: '492523411154281'});
             _.bindAll(this, 'renderWithUserProfCallback');
-        }/*,
-        submit: function(e) {
-            if (e.isDefaultPrevented()) return;
-            e.preventDefault();
-
-            var name = $('#banner_contact_full_name').val();
-            var email = $('#banner_contact_email_id').val();
-            var phone = $('#banner_contact_contact_num').val();
-            var propertyName = $('#banner_contact_property_name').val();
-            var query = $('#banner_contact_requirement').val();
-            var floorplan = $("#banner_contact_floorplan").prop('files')[0];
-
-            ConsultUtil.submit(name, email, phone, query, floorplan, propertyName, null, null);
-
-            window.App.router.navigate('/thankyou-contact-banner', {
-                trigger: true
-            });
-
-        },
-        events: {
-            "submit #banner_contactForm": "submit"
-        }*/
+        }
     });
     return DashboardPage;
 });
