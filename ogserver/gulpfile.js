ffb3911158bbdbf3b598382e6c97d890c549d888
Nitin Puri <univermal@gmail.com>
2016-07-13 12:36:19 +0530

    Revert: css combining (7d49ccd0b65050cd3f0cc8ee0097e44dcf79435d)

--- ogserver/gulpfile.js
+++ ogserver/gulpfile.js
@@ -1,29 +1,18 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
-var concat = require ('gulp-concat-css');
 
-gulp.task('uglifyjs', function() {
-  return gulp.src(['build/resources/main/webroot/js/**/*.js', '!build/resources/main/webroot/js/libs/**/*.js'])
+gulp.task('uglify', function() {
+  return gulp.src(['src/main/resources/webroot/js/**/*.js', '!src/main/resources/webroot/js/libs/**/*.js'])
     .pipe(uglify())
     .pipe(gulp.dest('build/resources/main/webroot/js'));
 });
 
-gulp.task('css', function(){
-    return gulp.src([
-    'build/resources/main/webroot/js/libs/bootstrap-custom/css/bootstrap.min.css',
-    'build/resources/main/webroot/js/libs/bootstrap-custom/css/bootstrap-theme.min.css',
-    'build/resources/main/webroot/js/libs/jquery-ui/themes/base/jquery-ui.min.css',
-    'build/resources/main/webroot/css/agency.css',
-    'build/resources/main/webroot/css/horizontal.css',
-    'build/resources/main/webroot/css/font-awesome.min.css',
-    'build/resources/main/webroot/fonts/pixeden/pe-icon-7-stroke/css/pe-icon-7-stroke.css',
-    'build/resources/main/webroot/fonts/pixeden/pe-icon-7-stroke/css/helper.css'
-], { base: 'build/resources/main/webroot/css' })
-.pipe(concat('single.css'))
-.pipe(cssnano({safe: true}))
-.pipe(gulp.dest('build/resources/main/webroot/css'));
+gulp.task('cssnano', function() {
+  return gulp.src('src/main/resources/webroot/css/**/*.css')
+    .pipe(cssnano({safe: true}))
+    .pipe(gulp.dest('build/resources/main/webroot/css'));
 });
 
-gulp.task('default', ['uglifyjs', 'css']);
+gulp.task('default', ['uglify', 'cssnano']);
 
