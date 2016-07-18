b1b725674aac57f668895dd97823c1251e1fc88d
Apeksha S <sapeksha43@gmail.com>
2016-07-18 13:22:34 +0530

    Game Data Setup

---
+++ ogserver/src/main/java/com/mygubbi/game/datasetup/AccessoryPackModuleMappingHandler.java
@@ -0,0 +1,43 @@
+package com.mygubbi.game.datasetup;
+
+import com.mygubbi.common.StringUtils;
+import com.mygubbi.si.excel.ExcelRowHandler;
+
+/**
+ * Created by Sunil on 05-05-2016.
+ */
+public class AccessoryPackModuleMappingHandler implements ExcelRowHandler
+{
+    @Override
+    public void handle(Object[] data)
+    {
+        String accode = (String) data[1];
+        if (StringUtils.isEmpty( accode) || "Accessory Pack Code".equals( accode)) return;
+
+        this.loopForComponents(data,  accode, 2, 21);
+
+    }
+
+    private void loopForComponents(Object[] data, String accode, int startColumn, int endColumn)
+    {
+        for (int i = startColumn; i <= endColumn; i = i+2)
+        {
+            String mgcode = (String) data[i];
+            if (StringUtils.isEmpty(mgcode)) continue;
+            String kdmaxcode = (String) data[i+1];
+            this.printRow(accode, mgcode, kdmaxcode);
+        }
+    }
+
+    private void printRow(String accode, String mgcode, String kdmaxcode)
+    {
+        System.out.println(accode + "|" + mgcode + "|" + kdmaxcode);
+    }
+
+    @Override
+    public void done()
+    {
+        System.out.println("Done");
+    }
+
+}
--- ogserver/src/main/java/com/mygubbi/game/datasetup/GameDataPreparer.java
+++ ogserver/src/main/java/com/mygubbi/game/datasetup/GameDataPreparer.java
@@ -7,16 +7,40 @@ import com.mygubbi.si.excel.ExcelReaderService;
  */
 public class GameDataPreparer
 {
-
-    public static final String XL_FILE = "D:\\work\\mygubbi\\game-2\\Master Working Sheet_Kitchen_KD Max - R1.xlsx";
+    public static final String XL_FILE = "D:\\MyGubbi\\SQL\\New\\Master Working Sheet_Kitchen_KD Max - R1.xlsx";
 
     public static void main(String[] args)
     {
+        GameDataPreparer dataPreparer = new GameDataPreparer();
+        dataPreparer.processMasterSheet();
+        //dataPreparer.processAccPackSheet();
+        //dataPreparer.processAccPackmappingSheet();
+    }
+
+    private void processMasterSheet() {
         int[] columnsToRead = new int[66];
         for (int i = 0; i<columnsToRead.length; i++)
         {
             columnsToRead[i] = i;
         }
         new ExcelReaderService(XL_FILE, 0, true, columnsToRead, new MasterSheetHandler()).read();
+    }
+
+    private void processAccPackSheet() {
+        int[] columnsToRead = new int[19];
+        for (int i = 0; i<columnsToRead.length; i++)
+        {
+            columnsToRead[i] = i;
+        }
+        new ExcelReaderService(XL_FILE, 1, true, columnsToRead, new AccessoryPackHandler()).read();
+    }
+
+    private void processAccPackmappingSheet() {
+        int[] columnsToRead = new int[22];
+        for (int i = 0; i<columnsToRead.length; i++)
+        {
+            columnsToRead[i] = i;
+        }
+        new ExcelReaderService(XL_FILE, 2, true, columnsToRead, new AccessoryPackModuleMappingHandler()).read();
     }
 }
