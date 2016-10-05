30328b477d95ddb79858f217c37387cf2dbe04d3
Mehbub <mehaboob.basha@orangegubbi.com>
2016-10-05 14:53:31 +0530

    Stories Dynamic in home page

--- ogserver/src/main/resources/webroot/js/views/dashboard/new-page.js
+++ ogserver/src/main/resources/webroot/js/views/dashboard/new-page.js
@@ -24,7 +24,7 @@ define([
             }));
             $.cloudinary.responsive();
             this.getStories();
-            $("img").unveil();
+            //$("img").unveil();
             this.ready();
 
         },
