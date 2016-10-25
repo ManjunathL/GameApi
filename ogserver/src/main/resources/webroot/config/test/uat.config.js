32e13b067c2443a2c501d0c6fcba2ca6b71468f1
nagmanibhushan <nagmani.bhushan@mygubbi.com>
2016-10-25 17:35:21 +0530

    ananlytics

--- ogserver/src/main/resources/webroot/config/test/uat.config.js
+++ ogserver/src/main/resources/webroot/config/test/uat.config.js
@@ -3,5 +3,5 @@ var restBase = "";
 var fbaseRootUrl = "https://sweltering-fire-6356.firebaseio.com/";
 //var mgAtatusCode = 'd46474f5f59d4c469dd22c853ff141bd';
 var mgAtatusCode = 'faad5014ff034809bb7522e4f47b85e1';
-var applyAnalytics = true;
+var applyAnalytics = false;
 var appendRobotsNoFollow = true;
