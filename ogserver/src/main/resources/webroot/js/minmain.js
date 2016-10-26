6f06780f2a804e013e60611b3c57bf4aeff8b22c
smrutiog <smruti.m@orangegubbi.com>
2016-10-26 16:34:53 +0530

    Fixed Search issue

---
+++ ogserver/src/main/resources/webroot/js/minmain.js
@@ -0,0 +1,63 @@
+require.config({
+        baseUrl: "js",
+        paths: {
+        jquery: 'libs/jquery/dist/jquery.min',
+        jqueryui: 'libs/jquery-ui/jquery-ui',
+        underscore: 'libs/underscore/underscore-min',
+        backbone: 'libs/backbone/backbone-min',
+        text: 'libs/text/text',
+        jqueryeasing: 'libs/jquery.easing/js/jquery.easing.min',
+        sly: 'libs/sly/dist/sly',
+        bxslider: 'libs/bxslider-4/jquery.bxslider.min',
+        bootstrap: 'libs/bootstrap-custom/js/bootstrap.min',
+        bootstrapvalidator: 'libs/bootstrap-validator/dist/validator.min',
+        firebase: 'libs/firebase/firebase',
+        cloudinary_jquery: 'libs/cloudinary-jquery.mg',
+        unveil: 'libs/unveil/jquery.unveil.mg',
+        templates: '../templates'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery', 'jqueryui'],
+            exports: 'Bootstrap'
+        },
+        'bootstrapvalidator': {
+            deps: ['bootstrap', 'jquery'],
+            exports: 'Bootstrapvalidator'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: function () {
+                return Backbone.noConflict();
+            }
+        },
+        'firebase': {
+            deps: ['jquery'],
+            exports: 'firebase'
+        },
+        'cloudinary_jquery': {
+            deps: ['jquery'],
+            exports: 'cloudinary-jquery'
+        },
+        'unveil': {
+            deps: ['jquery'],
+            exports: 'unveil'
+        }
+    },
+    map: {
+        '*': {
+            'css': 'libs/require-css/css'
+        }
+    }
+});
+
+require([
+    'views/app',
+    'router'
+], function (AppView, Router) {
+    var appView = new AppView();
+    appView.render();
+    Router.initialize({"appView": appView});
+});
+
+var imgBase = "https://res.cloudinary.com/mygubbi/image/upload/f_auto/";
