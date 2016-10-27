a01c918d497bf60f1a6b4128c3e6db5a63c00e9f
nagmanibhushan <nagmani.bhushan@mygubbi.com>
2016-10-27 16:51:04 +0530

    kitchen page

--- ogserver/src/main/resources/webroot/js/views/kitchentypes/typeofkitchen.js
+++ ogserver/src/main/resources/webroot/js/views/kitchentypes/typeofkitchen.js
@@ -5,71 +5,99 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!templates/typeofkitchen/typeofkitchen.html',
+    'text!templates/kitchentypes/typeofkitchen.html',
     'cloudinary_jquery',
     'slyutil',
     'mgfirebase',
     'consultutil',
     'analytics'
-], function($, _, Backbone, TypeOfKitchenPageTemplate, CloudinaryJquery, SlyUtil, MGF, ConsultUtil, Analytics) {
-    var TypeOfKitchenPageVIew = Backbone.View.extend({
-        el: '.page',
-        ref: null,
-        renderWithUserProfCallback: function(userProfData) {
-            $(this.el).html(_.template(TypeOfKitchenPageTemplate)({
-                'userProfile': userProfData
-            }));
-            $.cloudinary.responsive();
-        },
-        render: function() {
-            var authData = this.ref.getAuth();
-            MGF.getUserProfile(authData, this.renderWithUserProfCallback);
-        },
-        initialize: function() {
-            this.ref = MGF.rootRef;
-            Analytics.apply(Analytics.TYPE_GENERAL);
-            $.cloudinary.config({ cloud_name: 'mygubbi', api_key: '492523411154281'});
-            _.bindAll(this, 'renderWithUserProfCallback');
-        },
-        events: {
-            "click .base-lnk": "changebasetab",
-            "click .wall-lnk": "changewalltab",
-            "click .tall-lnk": "changetalltab"
-        },
-        changebasetab: function(e) {
-            e.preventDefault();
-            var currentTarget = $(e.currentTarget);
-            var id = $(e.currentTarget).attr('id');
-            $(".base-img").removeClass('active');
-             $(".base-cnt").removeClass('active');
-                $("#"+id+"-img").addClass('active');
-            $("#"+id+"-content").addClass('active');
+], function($, _, Backbone, kitchenTypePageTemplate, CloudinaryJquery, SlyUtil, MGF, ConsultUtil, Analytics) {
+    var kitchenTypePageVIew = Backbone.View.extend({
+            el: '.page',
+            ref: null,
+            renderWithUserProfCallback: function(userProfData){
+                $(this.el).html(_.template(kitchenTypePageTemplate)({
+                    'userProfile': userProfData
+                }));
+                $.cloudinary.responsive();
+            },
+            render: function(){
+                var authData = this.ref.getAuth();
+                MGF.getUserProfile(authData, this.renderWithUserProfCallback);
+            },
+            initialize: function(){
+                this.ref = MGF.rootRef;
+                Analytics.apply(Analytics.TYPE_GENERAL);
+                $.cloudinary.config({ cloud_name: 'mygubbi', api_key: '492523411154281'});
+                _.bindAll(this, 'renderWithUserProfCallback');
+            },
+            events: {
+                "click .hob-lnk": "changehobtab",
+                "click .chimney-lnk": "changechimneytab",
+                "click .apl-lnk": "changeapltab",
+                "click .sink-lnk": "changesinktab",
+                "click .faucet-lnk": "changefaucettab"
+            },
+            changehobtab: function(e){
+                e.preventDefault();
+                var currentTarget = $(e.currentTarget);
+                var id = $(e.currentTarget).attr('id');
+                $(".hob-img").removeClass('active');
+                 $(".hob-cnt").removeClass('active');
+                    $("#"+id+"-img").addClass('active');
+                $("#"+id+"-content").addClass('active');
 
-            return this;
-        },
-        changewalltab: function(e) {
-            e.preventDefault();
-            var currentTarget = $(e.currentTarget);
-            var id = $(e.currentTarget).attr('id');
-            $(".wall-img").removeClass('active');
-             $(".wall-cnt").removeClass('active');
-                $("#"+id+"-img").addClass('active');
-            $("#"+id+"-content").addClass('active');
+                console.log(id);
+                return this;
+            },
+            changechimneytab: function(e){
+                e.preventDefault();
+                var currentTarget = $(e.currentTarget);
+                var id = $(e.currentTarget).attr('id');
+                $(".chimney-img").removeClass('active');
+                 $(".chimney-cnt").removeClass('active');
+                    $("#"+id+"-img").addClass('active');
+                $("#"+id+"-content").addClass('active');
 
-            return this;
-        },
-        changetalltab: function(e) {
-            e.preventDefault();
-            var currentTarget = $(e.currentTarget);
-            var id = $(e.currentTarget).attr('id');
-            $(".tall-img").removeClass('active');
-             $(".tall-cnt").removeClass('active');
-                $("#"+id+"-img").addClass('active');
-            $("#"+id+"-content").addClass('active');
+                console.log(id);
+                return this;
+            },
+            changeapltab: function(e){
+                e.preventDefault();
+                var currentTarget = $(e.currentTarget);
+                var id = $(e.currentTarget).attr('id');
+                $(".apl-img").removeClass('active');
+                 $(".apl-cnt").removeClass('active');
+                    $("#"+id+"-img").addClass('active');
+                $("#"+id+"-content").addClass('active');
 
-            console.log(id);
-            return this;
-        }
-    });
-    return TypeOfKitchenPageVIew;
+                console.log(id);
+                return this;
+            },
+            changesinktab: function(e){
+                e.preventDefault();
+                var currentTarget = $(e.currentTarget);
+                var id = $(e.currentTarget).attr('id');
+                $(".sink-img").removeClass('active');
+                 $(".sink-cnt").removeClass('active');
+                    $("#"+id+"-img").addClass('active');
+                $("#"+id+"-content").addClass('active');
+
+                console.log(id);
+                return this;
+            },
+            changefaucettab: function(e){
+                e.preventDefault();
+                var currentTarget = $(e.currentTarget);
+                var id = $(e.currentTarget).attr('id');
+                $(".faucet-img").removeClass('active');
+                 $(".faucet-cnt").removeClass('active');
+                    $("#"+id+"-img").addClass('active');
+                $("#"+id+"-content").addClass('active');
+
+                console.log(id);
+                return this;
+            }
+        });
+        return kitchenTypePageVIew;
 });
