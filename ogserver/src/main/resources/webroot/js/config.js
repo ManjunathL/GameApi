c63c12236a46ace10e161cf6fb32ea1d4c919ba5
Mehbub <mehaboob.basha@orangegubbi.com>
2016-12-06 11:02:14 +0530

    New Uat config

--- ogserver/src/main/resources/webroot/js/config.js
+++ ogserver/src/main/resources/webroot/js/config.js
@@ -1,5 +1,5 @@
 var env = "dev";
-var restBase = "https://52.74.136.198";
+var restBase = "https://52.220.90.92";
 //var restBase = "https://localhost";
 var fbaseRootUrl = "https://sweltering-fire-6356.firebaseio.com/";
 var applyAnalytics = false;
