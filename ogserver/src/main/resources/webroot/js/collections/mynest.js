be188c2f39fd9367705a500fe6d413da7881dc1b
smrutiog <smruti.m@orangegubbi.com>
2016-12-06 13:44:27 +0530

    Update mynest

---
+++ ogserver/src/main/resources/webroot/js/collections/mynest.js
@@ -0,0 +1,21 @@
+define([
+ 'jquery',
+ 'underscore',
+ 'backbone',
+ 'models/mynest'
+], function($, _, Backbone, MyNest){
+   var MyNest = Backbone.Collection.extend({
+       model: MyNest,
+        urlRoot:'https://gameuat.mygubbi.com:1444' + '/gapi/outboundCrm//getOpportunityDetails',
+       initialize: function(models) {
+         _.each(models, function (mynest){
+           mynest = new MyNest(mynest);
+               console.log("collection");
+
+         });
+          console.log("=======mynest=======");
+                     console.log(mynest);
+       }
+   });
+ return MyNest;
+});
---
+++ ogserver/src/main/resources/webroot/js/models/mynest.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Mehbub on 21/11/16.
+ */
+
+define([
+    'jquery',
+    'backbone'
+], function($, Backbone) {
+    var MyNest = Backbone.Model.extend({
+        urlRoot:'https://gameuat.mygubbi.com:1444' + '/gapi/outboundCrm/getOpportunityDetails',
+        defaults: {
+          emailId: ''
+        }
+    });
+    console.log("model");
+    return MyNest;
+});
