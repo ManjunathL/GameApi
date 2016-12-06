601fddcdc9cde9389a0be804d496a7ab1eceb02f
Mehbub <mehaboob.basha@orangegubbi.com>
2016-12-06 18:37:25 +0530

    Email Updated with LOGS

--- ogserver/src/main/resources/webroot/js/config.js
+++ ogserver/src/main/resources/webroot/js/config.js
@@ -1,5 +1,5 @@
 var env = "dev";
-var restBase = "https://52.220.90.92";
+var restBase = "https://localhost:8000";
 //var restBase = "https://localhost";
 var fbaseRootUrl = "https://sweltering-fire-6356.firebaseio.com/";
 var applyAnalytics = false;
