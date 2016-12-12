0236366b7961848e7718508dba915cec1b1eab6d
smrutiog <smruti.m@orangegubbi.com>
2016-12-12 15:08:07 +0530

    Update product url

--- ogserver/src/main/java/com/mygubbi/route/BlogHandler.java
+++ ogserver/src/main/java/com/mygubbi/route/BlogHandler.java
@@ -19,14 +19,14 @@ import org.apache.logging.log4j.Logger;
  */
 public class BlogHandler extends AbstractRouteHandler
 {
-    private final static Logger LOG = LogManager.getLogger(ProductHandler.class);
+    private final static Logger LOG = LogManager.getLogger(BlogHandler.class);
 
     public BlogHandler(Vertx vertx)
     {
         super(vertx);
         this.route().handler(BodyHandler.create());
         this.get("/").handler(this::getAll); //Category, Sub-Category
-
+        this.get("/latestTag").handler(this::getLatest); //Category, Sub-Category
     }
 
     private void getAll(RoutingContext context) {
@@ -44,6 +44,7 @@ public class BlogHandler extends AbstractRouteHandler
 
                 JsonObject params = new JsonObject().put("tags", blogTags);
                 this.fetchProductsAndSend(context, "product.select.blogTags", params);
+
             }
         }
         else{
@@ -52,7 +53,41 @@ public class BlogHandler extends AbstractRouteHandler
         }
 
     }
+    private void getLatest(RoutingContext context) {
+
+        String blogTags = context.request().getParam("tags");
+
+        if (StringUtils.isNonEmpty(blogTags)) {
+            JsonObject params = new JsonObject().put("tags", blogTags);
+            this.fetchLatestProductsAndSend(context, "product.select.blogTagsLatest", params);
+        }
+        else{
+
+            sendError(context," Error:" + "Did not find blogs because" + " " + "no tags selected");
+        }
+
+    }
     private void fetchProductsAndSendb(RoutingContext context, String queryId, JsonObject paramsData)
+    {
+        Integer id = LocalCache.getInstance().store(new QueryData(queryId, paramsData));
+        LOG.info("Executing query:" + queryId + " | " + paramsData);
+        VertxInstance.get().eventBus().send(DatabaseService.DB_QUERY, id,
+                (AsyncResult<Message<Integer>> selectResult) -> {
+                    QueryData selectData = (QueryData) LocalCache.getInstance().remove(selectResult.result().body());
+                    if (selectData == null || selectData.rows == null)
+                    {
+                        sendError(context, "Did not find products for " + paramsData.toString() + ". Error:" + selectData.errorMessage);
+                    }
+                    else
+                    {
+                        //  this.fetchLatestProductsAndSend(context, "product.select.blogTagsLatest", paramsData);
+
+                        sendJsonResponse(context, selectData.getJsonDataRows("blogJson").toString());
+                    }
+                });
+
+    }
+    private void fetchLatestProductsAndSend(RoutingContext context, String queryId, JsonObject paramsData)
     {
         Integer id = LocalCache.getInstance().store(new QueryData(queryId, paramsData));
         LOG.info("Executing query:" + queryId + " | " + paramsData);
@@ -86,6 +121,9 @@ public class BlogHandler extends AbstractRouteHandler
                     {
 
                         sendJsonResponse(context, selectData.getJsonDataRows("blogJson").toString());
+
+                        //                     context.response().putHeader("content-type", "application/json").end(selectData.getJsonDataRows("productJson").encodePrettily());
+
                     }
                 });
 
