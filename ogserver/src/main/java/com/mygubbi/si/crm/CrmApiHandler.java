c7b5d033d36e14982046f7233e1d93d400964a86
Mehbub <mehaboob.basha@orangegubbi.com>
2016-12-26 12:40:55 +0530

    First name & full name

--- ogserver/src/main/java/com/mygubbi/si/crm/CrmApiHandler.java
+++ ogserver/src/main/java/com/mygubbi/si/crm/CrmApiHandler.java
@@ -311,7 +311,11 @@ public class CrmApiHandler extends AbstractRouteHandler
            // String password = RandomStringUtils.random(8, true, true);
             String password = "mygubbi";
             //String name = userJson.getString("email");
-            String name = userJson.getString("first_name");
+            //String name = userJson.getString("first_name");
+            String fullName = userJson.getString("first_name");
+            // Get the index of the first space.
+            int firstSpaceIndex = fullName.indexOf(" ");
+            String name = fullName.substring(0, firstSpaceIndex);
            String phone =  userJson.getString("mobile");
            // String phone =  userJson.getString("userId");
             String decodedEmail = URLDecoder.decode(email, StandardCharsets.UTF_8.name());
