8d22ef4425576db230736c24bc5def053278158f
Mehbub <mehaboob.basha@orangegubbi.com>
2016-12-27 00:45:57 +0530

    Email Updated with LOGS

--- ogserver/src/main/resources/webroot/js/config.js
+++ ogserver/src/main/resources/webroot/js/config.js
@@ -1,5 +1,6 @@
 var env = "dev";
-var restBase = "https://52.220.90.92";
+//var restBase = "https://52.220.90.92";
+var restBase = "https://mygubbi.com";
 //var restBase = "https://localhost";
 var fbaseRootUrl = "https://sweltering-fire-6356.firebaseio.com/";
 var applyAnalytics = false;
