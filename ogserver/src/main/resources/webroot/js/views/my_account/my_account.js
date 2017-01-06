353506ab8c83a6d522d448d6c467c9ad6b62134a
smruti <smruti.m@orangegubbi.com>
2017-01-06 12:20:53 +0530

    update client project status

--- ogserver/src/main/resources/webroot/js/views/my_account/my_account.js
+++ ogserver/src/main/resources/webroot/js/views/my_account/my_account.js
@@ -33,6 +33,7 @@ define([
         if(typeof(mynestitems) !== 'undefined' && mynestitems !== null){
 
             var project_status = mynestitems.paymentDetails[0].sales_stage;
+            var client_project_status_c = mynestitems.paymentDetails[0].client_project_status_c;
             var project_statusArr = new Array();
             switch (project_status) {
                 case "PROSPECT":case "FLOOR_PLAN_UPLOADED":case "INITIAL_PROPOSAL_UPLOADED":case "INITIAL_PROPOSAL_SENT":
@@ -41,18 +42,29 @@ define([
                 case "COLLECT_BOOKING_AMOUNT":case "COLLECTED_BOOKING_AMOUNT":case "SITE_MEASUREMENT_UPLOADED":case "DETAILED_DESIGN_APPROVED ":case "FINAL_PROPOSAL_UPLOADED":case "FINAL_PROPOSAL_SENT":case "COLLECT_ORDER_AMOUNT":
                     project_statusArr = ["initiated","proposal approved"];
                     break;
-                case "Closed_Won":case "Project Initiated":case "Upload Scope Document - Conduct 'Kick off' meeting - completed":case "Upload Workshop Drawing - completed":case "Upload Prod drawing signed-off by customer - completed":case "Upload Pre-Installation checklist & list site work - completed":case "Complete Preinstallation site work - completed":case "Generate SO extract & update ERP - completed":
+                case "Closed_Won":
                     project_statusArr = ["initiated","proposal approved","order placed"];
                     break;
-                case "Generate PO extract & update ERP - completed":case "Update PO details in CRM & Confirm delivery dates - completed":case "Update Product readiness for inspection  - completed":case "Upload the QC report post vendor site inspection  - completed":case "Final Payment Collection - completed":case "Update GRNs against the POs - completed":case "Upload product photos to confirm readiness for Delivery - completed":case "Update DO completion status - Completed":case "Upload Invoices - Completed":case "Upload Packing list and Accessories list - Completed":case "Update Site delivery status - Completed":
-                      project_statusArr = ["initiated","proposal approved","order placed","production started"];
-                      break;
-                case "Update Site Installation status - Completed":case "Upload Snaglist and confirm QC completion - Completed":
-                      project_statusArr = ["initiated","proposal approved","order placed","production started","installation"];
-                      break;
-                case "Upload Handover document and update Project Closure status - Completed":
-                      project_statusArr = ["initiated","proposal approved","order placed","production started","installation","handed over"];
-                      break;
+            }
+
+            console.log('-------------client_project_status_c---------------');
+            console.log(client_project_status_c);
+            if(typeof(client_project_status_c) != 'undefined' && client_project_status_c != null){
+
+                switch (client_project_status_c) {
+                    case "Project Initiated":case "Upload Scope Document - Conduct 'Kick off' meeting - completed":case "Upload Workshop Drawing - completed":case "Upload Prod drawing signed-off by customer - completed":case "Upload Pre-Installation checklist & list site work - completed":case "Complete Preinstallation site work - completed":case "Generate SO extract & update ERP - completed":
+                        project_statusArr = ["initiated","proposal approved","order placed"];
+                        break;
+                    case "Generate PO extract & update ERP - completed":case "Update PO details in CRM & Confirm delivery dates - completed":case "Update Product readiness for inspection  - completed":case "Upload the QC report post vendor site inspection  - completed":case "Final Payment Collection - completed":case "Update GRNs against the POs - completed":case "Upload product photos to confirm readiness for Delivery - completed":case "Update DO completion status - Completed":case "Upload Invoices - Completed":case "Upload Packing list and Accessories list - Completed":case "Update Site delivery status - Completed":
+                          project_statusArr = ["initiated","proposal approved","order placed","production started"];
+                          break;
+                    case "Update Site Installation status - Completed":case "Upload Snaglist and confirm QC completion - Completed":
+                          project_statusArr = ["initiated","proposal approved","order placed","production started","installation"];
+                          break;
+                    case "Upload Handover document and update Project Closure status - Completed":
+                          project_statusArr = ["initiated","proposal approved","order placed","production started","installation","handed over"];
+                          break;
+                }
             }
             console.log('-------- project_statusArr  -----------');
             console.log(project_statusArr);
@@ -212,32 +224,47 @@ define([
 
             var authData = this.refAuth.currentUser;
             var email = MGF.getEmail(authData);
+            var oldpassword = $('#old_password').val();
             var newPassword = $('#new_password').val();
 
-            authData.updatePassword(newPassword).then(function() {
-              // Update successful.
-              $('#success').html("User password changed successfully!");
-              $('#success_row').css("display", "block");
-              console.log("User password changed successfully!");
-            }, function(error) {
-              // An error happened.
-              switch (error.code) {
-                  case "INVALID_PASSWORD":
-                      $('#error').html("The specified user account password is incorrect.");
-                      $('#error_row').css("display", "block");
-                      console.log("The specified user account password is incorrect.");
-                      break;
-                  case "INVALID_USER":
-                      $('#error').html("The specified user account does not exist.");
-                      $('#error_row').css("display", "block");
-                      console.log("The specified user account does not exist.");
-                      break;
-                  default:
-                      $('#error').html("Error changing password:", error);
-                      $('#error_row').css("display", "block");
-                      console.log("Error changing password:", error);
-              }
-            });
+            console.log(email);
+            console.log('---------------oldPassword---------------');
+            console.log(oldpassword);
+            console.log('---------------newPassword---------------');
+            console.log(newPassword);
+
+            if((newPassword != null) && (oldpassword != newPassword)){
+               authData.reauthenticate(firebase.auth.EmailAuthProvider.credential(email, oldpassword));
+
+               authData.updatePassword(newPassword).then(function() {
+                    // Update successful.
+                    $('#success').html("User password changed successfully!");
+                    $('#success_row').css("display", "block");
+                    console.log("User password changed successfully!");
+                  }, function(error) {
+                    // An error happened.
+                    switch (error.code) {
+                        case "INVALID_PASSWORD":
+                            $('#error').html("The specified user account password is incorrect.");
+                            $('#error_row').css("display", "block");
+                            console.log("The specified user account password is incorrect.");
+                            break;
+                        case "INVALID_USER":
+                            $('#error').html("The specified user account does not exist.");
+                            $('#error_row').css("display", "block");
+                            console.log("The specified user account does not exist.");
+                            break;
+                        default:
+                            $('#error').html("Error changing password:", error);
+                            $('#error_row').css("display", "block");
+                            console.log("Error changing password:", error);
+                    }
+                  });
+            }else{
+                $('#error').html("Error changing password:", error);
+                $('#error_row').css("display", "block");
+                console.log("Error changing password:", error);
+            }
         },
         deactivateUserAccount: function (e) {
 
