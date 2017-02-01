b26e7f931c6bb739d089957c4985a5175a002af3
smruti <smruti.m@orangegubbi.com>
2017-02-01 14:22:25 +0530

    Update uat config

--- ogserver/src/main/resources/webroot/config/test/uat.config.js
+++ ogserver/src/main/resources/webroot/config/test/uat.config.js
@@ -1,27 +1,16 @@
 var env = "uat";
 var restBase = "";
 //var fbaseRootUrl = "https://sweltering-fire-6356.firebaseio.com/";
-//var fbaseRootUrl = "https://mygubbi-uat.firebaseio.com/";
-var fbaseRootUrl = "https://mygubbi.firebaseio.com/";
+var fbaseRootUrl = "https://mygubbi-uat.firebaseio.com/";
 //var mgAtatusCode = 'd46474f5f59d4c469dd22c853ff141bd';
 var mgAtatusCode = 'faad5014ff034809bb7522e4f47b85e1';
 var applyAnalytics = false;
 var appendRobotsNoFollow = true;
 
-/*
 var fbaseconfig = {
    apiKey: "AIzaSyCoKs1sqeE8GmMKysYpkOdO9KkZXcmhDQ0",
    authDomain: "mygubbi-uat.firebaseapp.com",
    databaseURL: "https://mygubbi-uat.firebaseio.com/",
    storageBucket: "mygubbi-uat.appspot.com",
    messagingSenderId: "106892346704"
- };*/
-
-
-var fbaseconfig = {
-   apiKey: "AIzaSyCkaDHeaIeltWiQ5QoBITO34sgwS149I1E",
-   authDomain: "mygubbi.firebaseapp.com",
-   databaseURL: "https://mygubbi.firebaseio.com",
-   storageBucket: "firebase-mygubbi.appspot.com",
-   messagingSenderId: "896609033641"
-};
+ };
