24cf56541faa056e130c28eb2feca1a58aeaeed4
Mehbub <mehaboob.basha@orangegubbi.com>
2017-03-31 11:15:47 +0530

    SEO Handlers, queries, config files

--- ogserver/src/main/java/com/mygubbi/config/StaticConfigHandler.java
+++ ogserver/src/main/java/com/mygubbi/config/StaticConfigHandler.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 public class StaticConfigHandler implements Handler<RoutingContext> {
 
     private static final String CONFIG_RESOURCE = "/js/config.js";
-    private static final String MAIN_JS_RESOURCE = "/js/main.js";
+    private static final String MAIN_JS_RESOURCE = "../js/main.js";
     private static final String IMPORTS_RESOURCE = "../imports.css";
     private static final String ROBOTS_RESOURCE = "/robots.txt";
 
