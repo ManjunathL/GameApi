7d8924216b08c51fb4be4768954ad66e46d13745
smruti <smruti.m@orangegubbi.com>
2017-04-10 15:41:15 +0530

    redirect product url

--- ogserver/src/main/java/com/mygubbi/apiserver/OldUrlRedirectHandler.java
+++ ogserver/src/main/java/com/mygubbi/apiserver/OldUrlRedirectHandler.java
@@ -1,9 +1,15 @@
 package com.mygubbi.apiserver;
 
+import com.mygubbi.common.LocalCache;
 import com.mygubbi.common.StringUtils;
+import com.mygubbi.common.VertxInstance;
 import com.mygubbi.config.ConfigHolder;
+import com.mygubbi.db.DatabaseService;
+import com.mygubbi.db.QueryData;
 import com.mygubbi.route.RouteUtil;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
@@ -22,7 +28,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class OldUrlRedirectHandler implements Handler<RoutingContext>
 {
     private final static Logger LOG = LogManager.getLogger(OldUrlRedirectHandler.class);
-    private static final String COLLECTIONS_URI = "/newproduct-details-";
+    private static final String COLLECTIONS_URI = "/product-";
     public static final int COLLECTIONS_URI_LENGTH = COLLECTIONS_URI.length();
 
     private final ConcurrentHashMap<RequestKey, ApiResponse> dataCache = new ConcurrentHashMap(256);
@@ -35,23 +41,23 @@ public class OldUrlRedirectHandler implements Handler<RoutingContext>
 
     private void prepareRedirectUrls()
     {
-         String test = "{\n" +
-                 "  \"oldurls\": [\n" +
-                 "    {\n" +
-                 "      \"old\": \"/bedroom-interior-design\",\n" +
-                 "      \"new\": \"/wardrobe-designs-online\"\n" +
-                 "    },\n" +
-                 "    {\n" +
-                 "      \"old\": \"/newproduct-details-\",\n" +
-                 "      \"new\": \"/product-\"\n" +
-                 "    },\n" +
-                 "    {\n" +
-                 "      \"old\": \"/living-and-dining-room-designs\",\n" +
-                 "      \"new\": \"/storage-solutions\"\n" +
-                 "    }\n" +
-                 "  ]\n" +
-                 "}\n" +
-                 "\n";
+        String test = "{\n" +
+                "  \"oldurls\": [\n" +
+                "    {\n" +
+                "      \"old\": \"/bedroom-interior-design\",\n" +
+                "      \"new\": \"/wardrobe-designs-online\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"old\": \"/newproduct-details-\",\n" +
+                "      \"new\": \"/product-\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"old\": \"/living-and-dining-room-designs\",\n" +
+                "      \"new\": \"/storage-solutions\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n" +
+                "\n";
         JsonObject jsobj = new JsonObject(test);
 
         //JsonArray shopifyUrls = (JsonArray) ConfigHolder.getInstance().getConfigValue("oldurls");
@@ -87,12 +93,43 @@ public class OldUrlRedirectHandler implements Handler<RoutingContext>
             return;
         }
 
+/*
         if (oldUrl.startsWith(COLLECTIONS_URI))
         {
             String oldURI = oldUrl;
             String newURI = oldURI.replace("/newproduct-details-","product-");
             LOG.info("Current url:" + newURI);
             RouteUtil.getInstance().redirect(context, newURI, "Redirecting to new mygubbi.com site");
+
+        }*/
+        if (oldUrl.startsWith(COLLECTIONS_URI))
+        {
+            String oldURI = oldUrl;
+            // String newURI = oldURI.replace("/newproduct-details-","product-");
+            LOG.info("Current url:" + oldUrl);
+            String productId= oldURI.substring(oldURI.indexOf('-')+1);
+            LOG.info("productId: " + productId);
+            Integer id = LocalCache.getInstance().store(new QueryData("product.select.productid", new JsonObject().put("productId", productId)));
+            VertxInstance.get().eventBus().send(DatabaseService.DB_QUERY, id,
+                    (AsyncResult<Message<Integer>> selectResult) -> {
+                        LOG.info("Executing query:" + "product.select.productid" + " | " + productId);
+                        QueryData selectData = (QueryData) LocalCache.getInstance().remove(selectResult.result().body());
+                        if (selectData == null || selectData.rows == null || selectData.rows.isEmpty())
+                        {
+                            LOG.error("sorry no product id found");
+                        }
+                        else
+                        {
+                            JsonArray jsonDataRows = selectData.getJsonDataRows("productJson");
+                            LOG.info(jsonDataRows.encodePrettily());
+                            JsonObject obj = jsonDataRows.getJsonObject(0);
+
+                            String newURI = "/" +obj.getString("seoId") + "/p/" + productId;
+                            LOG.info(newURI);
+                            RouteUtil.getInstance().redirect(context, newURI, "Redirecting to new product url");
+                        }
+                    });
+            return;
 
         }
 
