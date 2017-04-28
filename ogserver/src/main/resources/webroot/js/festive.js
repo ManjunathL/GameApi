a86875c8019fd6a2a5ed7b191ad3a62562496d9f
nagmani.bhushan <nagmani.bhushan@mygubbi.com>
2017-04-28 18:51:37 +0530

    landing page update

--- ogserver/src/main/resources/webroot/js/festive.js
+++ ogserver/src/main/resources/webroot/js/festive.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
                 items:1
             },
             600:{
-                items:2
+                items:1
             },
             1000:{
                 items:2
