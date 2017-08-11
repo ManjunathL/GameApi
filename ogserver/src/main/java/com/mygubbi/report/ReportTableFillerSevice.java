aeffe9622175355c7d2b61d5734f06adb8967a8d
sunil <sunil.guttula@wfglobal.org>
2017-08-11 12:04:02 +0530

    Async to Sync calls

--- ogserver/src/main/java/com/mygubbi/report/ReportTableFillerSevice.java
+++ ogserver/src/main/java/com/mygubbi/report/ReportTableFillerSevice.java
@@ -5,6 +5,8 @@ import com.mygubbi.common.VertxInstance;
 import com.mygubbi.db.DatabaseService;
 import com.mygubbi.db.QueryData;
 import com.mygubbi.game.proposal.model.ProposalVersion;
+import com.mygubbi.pipeline.MessageDataHolder;
+import com.mygubbi.pipeline.MultiMessageRequestExecutor;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
@@ -14,13 +16,14 @@ import io.vertx.core.json.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by shilpa on 10/8/17.
  */
-public class ReportTableFillerSevice extends AbstractVerticle{
+public class ReportTableFillerSevice extends AbstractVerticle
+{
     private final static Logger LOG = LogManager.getLogger(ReportTableFillerSevice.class);
     public static final String RUN_FOR_ALL_PROPOSALS = "reporting.table.filler.forAll";
     public static final String RUN_FOR_SINGLE_PROPOSAL = "reporting.table.filler.forOneProposal";
@@ -39,17 +42,18 @@ public class ReportTableFillerSevice extends AbstractVerticle{
         super.stop();
     }
 
-    private void runReportingTableFillingLogic(){
+    private void runReportingTableFillingLogic()
+    {
         EventBus eb = VertxInstance.get().eventBus();
         eb.localConsumer(RUN_FOR_ALL_PROPOSALS, (Message<Integer> message) -> {
-           this.getAllProposals(message);
+            this.getAllProposals(message);
         }).completionHandler(res -> {
             LOG.info("Proposal output service started." + res.succeeded());
         });
         eb.localConsumer(RUN_FOR_SINGLE_PROPOSAL, (Message<Integer> message) -> {
             JsonObject obj = (JsonObject) LocalCache.getInstance().remove(message.body());
-            LOG.info("Json Obj = "+obj);
-            this.getVersionObjForProposal(obj.getInteger("proposalId"),message);
+            LOG.info("Json Obj = " + obj);
+            this.getVersionObjForProposal(obj.getInteger("proposalId"), message);
         }).completionHandler(res -> {
             LOG.info("Proposal output service started." + res.succeeded());
         });
@@ -60,10 +64,11 @@ public class ReportTableFillerSevice extends AbstractVerticle{
         });
     }
 
-    private void getAllUpdatedProposals(Message message){
+    private void getAllUpdatedProposalsOld(Message message)
+    {
 
 //        List<Integer> proposalIds = new ArrayList<>();
-                Integer id = LocalCache.getInstance().store(new QueryData("proposal.updatedProposals.select", new JsonObject()));
+        Integer id = LocalCache.getInstance().store(new QueryData("proposal.updatedProposals.select", new JsonObject()));
         VertxInstance.get().eventBus().send(DatabaseService.DB_QUERY, id,
                 (AsyncResult<Message<Integer>> selectResult) -> {
                     QueryData resultData = (QueryData) LocalCache.getInstance().remove(selectResult.result().body());
@@ -76,9 +81,9 @@ public class ReportTableFillerSevice extends AbstractVerticle{
                     {
 
 //                        ProposalHeader proposalHeader = new ProposalHeader(resultData.rows.get(0));
-                        resultData.rows.forEach(item ->{
+                        resultData.rows.forEach(item -> {
 //                            proposalIds.add(item.getInteger("id"));
-                            getVersionObjForProposal(item.getInteger("id"),message) ;
+                            getVersionObjForProposal(item.getInteger("id"), message);
                         });
 
                     }
@@ -86,7 +91,30 @@ public class ReportTableFillerSevice extends AbstractVerticle{
 
     }
 
-    private void getAllProposals(Message message){
+    private void getAllUpdatedProposals(Message message)
+    {
+        Integer id = LocalCache.getInstance().store(new QueryData("proposal.updatedProposals.select", new JsonObject()));
+        VertxInstance.get().eventBus().send(DatabaseService.DB_QUERY, id,
+                (AsyncResult<Message<Integer>> selectResult) -> {
+                    QueryData resultData = (QueryData) LocalCache.getInstance().remove(selectResult.result().body());
+                    if (resultData.errorFlag || resultData.rows == null || resultData.rows.isEmpty())
+                    {
+                        message.reply(LocalCache.getInstance().store(new JsonObject().put("error", "Proposal table is empty")));
+                        LOG.error("Proposal table is empty");
+                    }
+                    else
+                    {
+                        resultData.rows.forEach(item -> {
+                            getVersionObjForProposal(item.getInteger("id"), message);
+                        });
+
+                    }
+                });
+
+    }
+
+    private void getAllProposals(Message message)
+    {
 
 //        List<Integer> proposalIds = new ArrayList<>();
         Integer id = LocalCache.getInstance().store(new QueryData("proposal.all.select", new JsonObject()));
@@ -100,11 +128,8 @@ public class ReportTableFillerSevice extends AbstractVerticle{
                     }
                     else
                     {
-
-//                        ProposalHeader proposalHeader = new ProposalHeader(resultData.rows.get(0));
-                        resultData.rows.forEach(item ->{
-//                            proposalIds.add(item.getInteger("id"));
-                            getVersionObjForProposal(item.getInteger("id"),message) ;
+                        resultData.rows.forEach(item -> {
+                            getVersionObjForProposal(item.getInteger("id"), message);
                         });
 
                     }
@@ -112,32 +137,53 @@ public class ReportTableFillerSevice extends AbstractVerticle{
 
     }
 
-    private void getVersionObjForProposal(Integer proposalId,Message message){
-        List<ProposalVersion> pvObjList = new ArrayList<>();
-      Integer id = LocalCache.getInstance().store(new QueryData("proposal.versions.list", new JsonObject().put("proposalId",proposalId)));
+    private void getVersionObjForProposalOld(Integer proposalId, Message message)
+    {
+        Integer id = LocalCache.getInstance().store(new QueryData("proposal.versions.list", new JsonObject().put("proposalId", proposalId)));
         VertxInstance.get().eventBus().send(DatabaseService.DB_QUERY, id,
                 (AsyncResult<Message<Integer>> selectResult) -> {
                     QueryData resultData = (QueryData) LocalCache.getInstance().remove(selectResult.result().body());
                     if (resultData.errorFlag || resultData.rows == null || resultData.rows.isEmpty())
                     {
-                        message.reply(LocalCache.getInstance().store(new JsonObject().put("error", "Proposal "+proposalId+" doesn't have any versions")));
-                        LOG.error("Proposal "+proposalId+" doesn't have any versions");
+                        message.reply(LocalCache.getInstance().store(new JsonObject().put("error", "Proposal " + proposalId + " doesn't have any versions")));
+                        LOG.error("Proposal " + proposalId + " doesn't have any versions");
                     }
                     else
                     {
-
-                      resultData.rows.forEach(item ->{
-                          Integer id1 = LocalCache.getInstance().store(new ProposalVersion(item));
-                          LOG.debug("Item :" + item.encodePrettily());
-                          VertxInstance.get().eventBus().send(DwReportingService.RECORD_VERSION_PRICE, id1,
-                                  (AsyncResult<Message<Integer>> result) -> {
-                                      JsonObject response = (JsonObject) LocalCache.getInstance().remove(result.result().body());
-                                      LOG.info("Quote Res :: " + response);
-                                      message.reply(response);
-                                  });
+                        resultData.rows.forEach(item -> {
+                            Integer id1 = LocalCache.getInstance().store(new ProposalVersion(item));
+                            LOG.debug("Item :" + item.encodePrettily());
+                            VertxInstance.get().eventBus().send(DwReportingService.RECORD_VERSION_PRICE, id1,
+                                    (AsyncResult<Message<Integer>> result) -> {
+                                        JsonObject response = (JsonObject) LocalCache.getInstance().remove(result.result().body());
+                                        LOG.info("Quote Res :: " + response);
+                                        message.reply(response);
+                                    });
 
                         });
 
+                    }
+                });
+    }
+
+    private void getVersionObjForProposal(Integer proposalId, Message message)
+    {
+        Integer id = LocalCache.getInstance().store(new QueryData("proposal.versions.list", new JsonObject().put("proposalId", proposalId)));
+        VertxInstance.get().eventBus().send(DatabaseService.DB_QUERY, id,
+                (AsyncResult<Message<Integer>> selectResult) -> {
+                    QueryData resultData = (QueryData) LocalCache.getInstance().remove(selectResult.result().body());
+                    if (resultData.errorFlag || resultData.rows == null || resultData.rows.isEmpty())
+                    {
+                        message.reply(LocalCache.getInstance().store(new JsonObject().put("error", "Proposal " + proposalId + " doesn't have any versions")));
+                        LOG.error("Proposal " + proposalId + " doesn't have any versions");
+                    }
+                    else
+                    {
+                        List<MessageDataHolder> messageDataHolders = resultData.rows.stream()
+                                .map(row -> new MessageDataHolder(DwReportingService.RECORD_VERSION_PRICE, new ProposalVersion(row)))
+                                .collect(Collectors.toList());
+                        List<MessageDataHolder> responseDataHolders = new MultiMessageRequestExecutor().execute(messageDataHolders);
+                        message.reply("Done");
                     }
                 });
     }
