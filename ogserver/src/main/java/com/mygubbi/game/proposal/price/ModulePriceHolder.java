b9f4dcd1638eaac263295f4a49996f0722975b8d
Chirag <chiragsharath@gmail.com>
2017-09-05 10:40:32 +0530

    -removal of logs

--- ogserver/src/main/java/com/mygubbi/game/proposal/price/ModulePriceHolder.java
+++ ogserver/src/main/java/com/mygubbi/game/proposal/price/ModulePriceHolder.java
@@ -125,14 +125,14 @@ public class ModulePriceHolder
 
     public ModulePriceHolder(ModuleForPrice moduleForPrice)
     {
-        LOG.debug("Module For price :" + moduleForPrice);
+//        LOG.debug("Module For price :" + moduleForPrice);
         this.productModule = moduleForPrice.getModule();
-        LOG.debug("This productModule : " + this.productModule);
+//        LOG.debug("This productModule : " + this.productModule);
         this.priceDate = moduleForPrice.getPriceDate();
         this.city = moduleForPrice.getCity();
         //this.handleType = moduleForPrice.getProduct().getHandletypeSelection();
         this.productLineItem = moduleForPrice.getProduct();
-        LOG.debug("This City : " + this.city);
+//        LOG.debug("This City : " + this.city);
     }
 
 
@@ -142,7 +142,7 @@ public class ModulePriceHolder
         this.city = city;
         this.priceDate = date;
         this.productLineItem = productLineItem;
-        LOG.debug("Inside 2nd Module price holder");
+//        LOG.debug("Inside 2nd Module price holder");
 
 
     }
