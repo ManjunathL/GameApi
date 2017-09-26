702614d4f6e86bd1da111e78629c25a21aed0f20
Shruthi <shruthirs30@gmail.com>
2017-09-26 12:56:38 +0530

    report changes

--- ogserver/src/main/java/com/mygubbi/game/proposal/model/dw/DWModuleComponent.java
+++ ogserver/src/main/java/com/mygubbi/game/proposal/model/dw/DWModuleComponent.java
@@ -705,8 +705,6 @@ public class DWModuleComponent extends JsonObject {
             }
         }
 
-        panelCost = panelCost - (panelCost * (proposalVersion.getDiscountPercentage()/100));
-
         panelPriceAfterDiscount = panelPrice - (panelPrice * (proposalVersion.getDiscountPercentage()/100));
         panelPriceWoTax = panelPriceAfterDiscount * prodWoTaxFactor.getSourcePrice();
         panelProfit = panelPriceWoTax - panelCost;
