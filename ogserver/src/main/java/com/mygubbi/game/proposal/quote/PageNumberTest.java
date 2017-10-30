0209e4d742a81e234e1d8c01354aadc91666adfa
Shruthi <shruthirs30@gmail.com>
2017-10-30 11:56:02 +0530

    - Page number test

--- ogserver/src/main/java/com/mygubbi/game/proposal/quote/PageNumberTest.java
+++ ogserver/src/main/java/com/mygubbi/game/proposal/quote/PageNumberTest.java
@@ -1,5 +1,8 @@
 package com.mygubbi.game.proposal.quote;
 
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
@@ -7,6 +10,9 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Rectangle;
 
 
 import java.io.File;
@@ -18,8 +24,10 @@ import java.io.File;
  */
 public class PageNumberTest
 {
-    public static final String SRC = "E:/WorkContract.pdf";
+    public static final String SRC = "E:/WorksContractQuoteFinal.pdf";
     public static final String DEST = "E:/WorkContractMergedPdf.pdf";
+    Font fsize=new Font(Font.FontFamily.TIMES_ROMAN,8,Font.NORMAL);
+    PdfFont font;
 
     public static void main(String[] args)
             throws Exception {
@@ -28,12 +36,25 @@ public class PageNumberTest
     }
 
     protected void manipulatePdf(String dest) throws Exception {
+        try
+        {
+            font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         PdfDocument pdfDoc = new PdfDocument(new PdfReader(SRC), new PdfWriter(DEST));
         Document doc = new Document(pdfDoc);
         int n = pdfDoc.getNumberOfPages();
         for (int i = 1; i <= n; i++) {
             //doc.showTextAligned(new Paragraph(String.format("page %s of %s", i, n)), 559, 810, i, TextAlignment.CENTER, VerticalAlignment.MIDDLE, 0);
-            doc.showTextAligned(new Paragraph(String.format("page %s of %s", i, n)), ((doc.getRightMargin()-doc.getLeftMargin())/2), doc.getBottomMargin()-20, i, TextAlignment.CENTER, VerticalAlignment.BOTTOM, 0);
+//            doc.showTextAligned(new Paragraph(String.format("page %s of %s", i, n)), ((doc.getRightMargin()-doc.getLeftMargin())/2), doc.getBottomMargin()-20, i, TextAlignment.CENTER, VerticalAlignment.BOTTOM, 0);
+            PageSize pageSize = new PageSize();
+            System.out.println("x: " +(doc.getBottomMargin() + 500) +"y: " +((doc.getLeftMargin()+doc.getRightMargin())/2));
+            //doc.showTextAligned(new Paragraph(String.format("page %s of %s", i, n)), doc.getBottomMargin() + 500, ((doc.getLeftMargin()+doc.getRightMargin())/2)-5, i, TextAlignment.CENTER, VerticalAlignment.BOTTOM, 0);
+            doc.showTextAligned(new Paragraph(String.format("Page %s of %s", i, n)).setFont(font), 536, 10, i, TextAlignment.CENTER, VerticalAlignment.BOTTOM, 0);
+
         }
         doc.close();
     }
