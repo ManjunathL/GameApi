7a1363d5750f01cb75b749a4cbfe83dd482e91f6
smruti <smruti.m@orangegubbi.com>
2018-03-23 10:19:46 +0530

    updated user preferences design

--- ogserver/src/main/resources/webroot/js/main.js
+++ ogserver/src/main/resources/webroot/js/main.js
@@ -63,5 +63,5 @@ var authbaseRestApiUrl = "http://45.112.138.146:8080/";
 var baseRestApiUrl = "http://45.112.138.146:8080/";
 
 
-//var authbaseRestApiUrl = "http://192.168.104.183:9000/";
-//var baseRestApiUrl = "http://192.168.104.183:9100/";
+//var authbaseRestApiUrl = "http://192.168.104.190:9000/";
+//var baseRestApiUrl = "http://192.168.104.190:9100/";
