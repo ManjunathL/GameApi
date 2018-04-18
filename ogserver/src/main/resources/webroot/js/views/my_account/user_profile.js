4f7df9683be0a603a82f327a9fb12f62bd7970d5
Shruthi <shruthirs30@gmail.com>
2018-04-18 10:43:49 +0530

    user preference changes,user profile changes

--- ogserver/src/main/resources/webroot/js/views/my_account/user_profile.js
+++ ogserver/src/main/resources/webroot/js/views/my_account/user_profile.js
@@ -3,8 +3,9 @@ define([
   'lodash',
   'backbone',
   'bootstrap',
-   'text!templates/my_account/user_profile.html'
-], function($, _, Backbone, Bootstrap, UserProfileTemplate){
+  'mgfirebase',
+  'text!templates/my_account/user_profile.html'
+], function($, _, Backbone, Bootstrap,MGF, UserProfileTemplate){
   var UserProfilePage = Backbone.View.extend({
     el: '.page',
     initialize: function() {
@@ -14,7 +15,55 @@ define([
     render: function () {
        var that = this;
        $(this.el).html(_.template(UserProfileTemplate));
-    }
+    },
+     events: {
+                "click #save_details": "submit"
+            },
+    submit: function (e) {
+                          if (e.isDefaultPrevented()) return;
+                          e.preventDefault();
+
+                          var communicationAddress={
+                                        "houseNoStreet": $('#inputCommunicationAddress').val(),
+                                        "city": $('#inputCommunicationCity').val(),
+                                        "state": $('#inputCommunicationState').val(),
+                                        "pincode": $('#inputCommunicationpincode').val()
+                          } ;
+
+                          var projectAddress={
+                                    "houseNoStreet": $('#inputProjectAddress').val(),
+                                    "city": $('#inputProjectCity').val(),
+                                    "state": $('#inputProjectState').val(),
+                                    "pincode": $('#inputpincode').val()
+                          };
+                          var projectDetails={
+                                    "builder": $('#inputbuilder').val(),
+                                    "city": $('#inputcity').val(),
+                                    "plan": $('#inputplan').val(),
+                                    "project": $('#inputproject').val(),
+                                    "projecttower": $('#inputprojecttower').val()
+                          };
+                          var formData = {
+                              "additionalEmail": $('#inputemail').val(),
+                              "communicataionAddress":communicationAddress,
+                              "dob": $('#inputdob').val(),
+                              /*"gender": $('#gender').val(),*/
+                              "gender": "female",
+                              "mobileNumber": $('#inputmobilenumber').val(),
+                              "name": $('#inputname').val(),
+                              "projectAddress":projectAddress,
+                              "projectDetail":projectDetails,
+                              "surName": $('#inputsurname').val()
+                          };
+                          console.log("form Data ");
+                          console.log(formData);
+                          var that = this;
+                          MGF.updateProfile(formData).then(function () {
+                          console.log("inside update Profile ");
+                              that.render()
+                          });
+                      }
+
   });
   return UserProfilePage;
 });
