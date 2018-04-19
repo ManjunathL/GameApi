82302d13d1ab04a4650be47d92244cdf2747c313
tushar.s <tushar.s@mygubbi.com>
2018-04-19 18:17:52 +0530

    Merge branch 'imagineer' of E:\myGubbi_project\Intellij Projects\Imaginee with conflicts.

--- ogserver/src/main/resources/webroot/js/views/my_account/user_preference.js
+++ ogserver/src/main/resources/webroot/js/views/my_account/user_preference.js
@@ -73,7 +73,8 @@ define([
                          resolve();
                       }
       });
-    },getUserFamilyPreferences: function(){
+    },
+    getUserFamilyPreferences: function(){
         var that = this;
         var category = 2;
         //var userId = sessionStorage.userId;
@@ -152,7 +153,10 @@ define([
     },
     events: {
         "click .userques": "getSelectedQuestion",
-        "click #save_homeId":"saveHomePreferences"
+        "click #save_homeId":"saveHomePreferences",
+        "click .userquesfamily":"getSelectedQuestionFamily",
+        "click #save_familyId":"saveFamilyPreferences"
+
     },
     saveHomePreferences:function(e){
         var that = this;
@@ -323,7 +327,7 @@ define([
         console.log(that.filter_preference.get("selectedUsage"));
 
 
-        var  user_home_preferences = that.user_home_preferences;
+        var  user_home_preferences = that.user_family_preferences;
         user_home_preferences = user_home_preferences.toJSON();
 
          console.log("@@@@@@@@@@@@@@ user_home_preferences @@@@@@@@@@@@@@@@@@@");
@@ -379,7 +383,289 @@ define([
          });
 
         return false;
-    }
+    },
+
+    saveFamilyPreferences:function(e){
+            var that = this;
+
+
+            if (e.isDefaultPrevented()) return;
+                    e.preventDefault();
+                     var userId = "2ZBLKQ4vGMRSuN7k8AH8nf7InG43";
+                     var formData = that.filter_preference.get("selectedFamilyPreference");
+
+                    var category = 1;
+                    console.log("++++++++++++++++++++++++++ formData ++++++++++++++++++++++++++++++");
+                    console.log(JSON.stringify(formData));
+
+
+                    that.saveuser_preferences.saveUserPreferences(category,userId,{
+                       async: true,
+                       crossDomain: true,
+                       method: "POST",
+                       headers:{
+                           "authorization": "Bearer "+ sessionStorage.authtoken,
+                           "Content-Type": "application/json"
+                       },
+                       data: JSON.stringify(formData),
+                       success:function(response) {
+                          console.log("Successfully saved Family Preferences");
+                          console.log(response);
+
+                          $("#snackbar").html("Successfully saved Family Preferences");
+                          var x = document.getElementById("snackbar")
+                          x.className = "show";
+                          setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+                          that.render();
+                          return;
+
+                      },
+                      error:function(model, response, options) {
+
+                          console.log(" +++++++++++++++ Home Preferences save- Errrorr ++++++++++++++++++ ");
+                          console.log(JSON.stringify(response));
+
+
+                           $("#snackbar").html(response.responseJSON.errorMessage);
+                           var x = document.getElementById("snackbar")
+                           x.className = "show";
+                           setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+                           return;
+
+                      }
+                  });
+        },
+    getSelectedQuestionFamily: function(evt){
+            evt.preventDefault();
+
+            var that = this;
+
+            var currentTarget = $(evt.currentTarget);
+            var quesId = currentTarget.data('element');
+            var quesOptionid = currentTarget.data('element1');
+            var quesName = currentTarget.data('element2');
+
+
+
+
+            var adults = {};
+            var teenagers  = {};
+            var kids = {};
+            var seniorCitizens = {};
+            var weWouldLikeOurHomeToBe = {};
+            var weLoveTo={};
+            var weUsuallyDo={};
+
+
+
+            if(typeof(quesName) !== 'undefined' && quesName == "Adults"){
+
+                adults['optionId'] = quesOptionid;
+                adults['optionType'] = 2;
+                adults['quesAnsId'] = quesOptionid;
+                adults['quesId'] = quesId;
+                adults['selectionStatus'] = 1;
+                adults['userOptionDesc'] = "Added by tushar";
+
+
+                that.filter_preference.set({
+                    'selectedAdults':adults
+                }, {
+                    silent: true
+                });
+
+            }
+
+             if(typeof(quesName) !== 'undefined' && quesName == "Teenagers / Young Adults"){
+
+                teenagers['optionId'] = quesOptionid;
+                teenagers['optionType'] = 2;
+                teenagers['quesAnsId'] = quesOptionid;
+                teenagers['quesId'] = quesId;
+                teenagers['selectionStatus'] = 1;
+                teenagers['userOptionDesc'] = "";
+
+                that.filter_preference.set({
+                    'selectedTeenagers':teenagers
+                }, {
+                    silent: true
+                });
+
+            }
+
+            if(typeof(quesName) !== 'undefined' && quesName == "Kids"){
+                kids['optionId'] = quesOptionid;
+                kids['optionType'] = 2;
+                kids['quesAnsId'] = quesOptionid;
+                kids['quesId'] = quesId;
+                kids['selectionStatus'] = 1;
+                kids['userOptionDesc'] = "";
+
+                that.filter_preference.set({
+                    'selectedKids':kids
+                }, {
+                    silent: true
+                });
+
+            }
+
+            if(typeof(quesName) !== 'undefined' && quesName == "Senior Citizens"){
+                seniorCitizens['optionId'] = quesOptionid;
+                seniorCitizens['optionType'] = 2;
+                seniorCitizens['quesAnsId'] = quesOptionid;
+                seniorCitizens['quesId'] = quesId;
+                seniorCitizens['selectionStatus'] = 1;
+                seniorCitizens['userOptionDesc'] = "";
+
+                that.filter_preference.set({
+                    'selectedSeniorCitizens':seniorCitizens
+                }, {
+                    silent: true
+                });
+
+            }
+
+            if(typeof(quesName) !== 'undefined' && quesName == "We would like our home to be"){
+                weWouldLikeOurHomeToBe['optionId'] = quesOptionid;
+                weWouldLikeOurHomeToBe['optionType'] = 2;
+                weWouldLikeOurHomeToBe['quesAnsId'] = quesOptionid;
+                weWouldLikeOurHomeToBe['quesId'] = quesId;
+                weWouldLikeOurHomeToBe['selectionStatus'] = 1;
+                weWouldLikeOurHomeToBe['userOptionDesc'] = "";
+
+                that.filter_preference.set({
+                    'selectedWeWouldLikeOurHomeToBe':weWouldLikeOurHomeToBe
+                }, {
+                    silent: true
+                });
+
+            }
+
+             if(typeof(quesName) !== 'undefined' && quesName == "We love to"){
+                weLoveTo['optionId'] = quesOptionid;
+                weLoveTo['optionType'] = 2;
+                weLoveTo['quesAnsId'] = quesOptionid;
+                weLoveTo['quesId'] = quesId;
+                weLoveTo['selectionStatus'] = 1;
+                weLoveTo['userOptionDesc'] = "";
+
+                that.filter_preference.set({
+                    'selectedWeLoveTo':weLoveTo
+                }, {
+                    silent: true
+                });
+
+             }
+             if(typeof(quesName) !== 'undefined' && quesName == "We usually do"){
+                  weUsuallyDo['optionId'] = quesOptionid;
+                  weUsuallyDo['optionType'] = 2;
+                  weUsuallyDo['quesAnsId'] = quesOptionid;
+                  weUsuallyDo['quesId'] = quesId;
+                  weUsuallyDo['selectionStatus'] = 1;
+                  weUsuallyDo['userOptionDesc'] = "";
+
+                  that.filter_preference.set({
+                      'selectedWeUsuallyDo':weUsuallyDo
+                  }, {
+                      silent: true
+                  });
+
+             }
+
+            console.log(" +++++++++++++++ quesId ++++++++++++++++++ ");
+            console.log(quesName);
+            console.log(quesId+"======================"+quesOptionid);
+
+            console.log("@@@@@@@@@@@@@@@@ Adult @@@@@@@@@@@@@@@@@");
+            console.log(that.filter_preference.get("selectedAdults"));
+
+            console.log("@@@@@@@@@@@@@@ Teenagers @@@@@@@@@@@@@@@@@@@");
+            console.log(that.filter_preference.get("selectedTeenagers"));
+
+            console.log("@@@@@@@@@@@@@@ Kids @@@@@@@@@@@@@@@@@@@");
+            console.log(that.filter_preference.get("selectedKids"));
+
+            console.log("@@@@@@@@@@@@@@ SeniorCitizens @@@@@@@@@@@@@@@@@@@");
+            console.log(that.filter_preference.get("selectedSeniorCitizens"));
+
+            console.log("@@@@@@@@@@@@@@ We Would Like Our Home To Be @@@@@@@@@@@@@@@@@@@");
+            console.log(that.filter_preference.get("selectedWeWouldLikeOurHomeToBe"));
+
+            console.log("@@@@@@@@@@@@@@ We Love To @@@@@@@@@@@@@@@@@@@");
+            console.log(that.filter_preference.get("selectedWeLoveTo"));
+
+            console.log("@@@@@@@@@@@@@@ We Usually Do @@@@@@@@@@@@@@@@@@@");
+            console.log(that.filter_preference.get("selectedWeUsuallyDo"));
+
+
+            var  user_family_preferences = that.user_family_preferences;
+            user_family_preferences = user_family_preferences.toJSON();
+
+             console.log("@@@@@@@@@@@@@@ user_family_preferences @@@@@@@@@@@@@@@@@@@");
+             console.log(user_family_preferences);
+
+             _.each(user_family_preferences[0].userQues, function(userQuesdtl, j){
+                    if(j == 0){
+                        if(typeof(that.filter_preference.get("selectedAdults")) !== 'undefined'){
+                            userQuesdtl.userSelectedOption[0] = that.filter_preference.get("selectedAdults");
+                        }
+                    }
+                    if(j == 1){
+                        if(typeof(that.filter_preference.get("selectedTeenagers")) !== 'undefined'){
+                            userQuesdtl.userSelectedOption[0] = that.filter_preference.get("selectedTeenagers");
+                        }
+                    }
+                    if(j == 2){
+                        if(typeof(that.filter_preference.get("selectedKids")) !== 'undefined'){
+                            userQuesdtl.userSelectedOption[0]= that.filter_preference.get("selectedKids");
+                        }
+                    }
+                    if(j == 3){
+                        if(typeof(that.filter_preference.get("selectedSeniorCitizens")) !== 'undefined'){
+                            userQuesdtl.userSelectedOption[0] = that.filter_preference.get("selectedSeniorCitizens");
+                        }
+                    }
+                    if(j == 4){
+                        if(typeof(that.filter_preference.get("selectedWeWouldLikeOurHomeToBe")) !== 'undefined'){
+                            userQuesdtl.userSelectedOption[0] = that.filter_preference.get("selectedWeWouldLikeOurHomeToBe");
+                        }
+                    }
+                     if(j == 5){
+                        if(typeof(that.filter_preference.get("selectedWeLoveTo")) !== 'undefined'){
+                            userQuesdtl.userSelectedOption[0] = that.filter_preference.get("selectedWeLoveTo");
+                        }
+                    }
+                    if(j == 6){
+                         if(typeof(that.filter_preference.get("selectedWeUsuallyDo")) !== 'undefined'){
+                             userQuesdtl.userSelectedOption[0] = that.filter_preference.get("selectedWeUsuallyDo");
+                         }
+                     }
+
+             });
+
+             var user_pref_for_saving = {};
+             user_pref_for_saving = user_family_preferences[0].userQues;
+
+             console.log("_____________Tushar LINE______________");
+             console.log(user_pref_for_saving);
+
+             console.log("@@@@@@@@@@@@@@ Final user_family_preferences object to save @@@@@@@@@@@@@@@@@@@");
+             console.log(user_family_preferences);
+
+              that.filter_preference.set({
+                             'selectedFamilyPreference':user_pref_for_saving
+                         }, {
+                             silent: true
+                         });
+
+             that.filter_preference.set({
+                 'selectedFinalObjFamily':user_family_preferences
+             }, {
+                 silent: true
+             });
+
+            return false;
+        }
   });
   return UserProfilePage;
 });
