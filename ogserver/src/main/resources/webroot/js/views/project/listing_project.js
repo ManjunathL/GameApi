2dc2086a461f5581de103fe3c32371e9dc445205
smruti <smruti.m@orangegubbi.com>
2018-05-29 14:53:10 +0530

    updated space type

--- ogserver/src/main/resources/webroot/js/views/project/listing_project.js
+++ ogserver/src/main/resources/webroot/js/views/project/listing_project.js
@@ -70,112 +70,116 @@ define([
             });
     },
     saveConceptFileupload: function(){
-                var that = this;
+        var that = this;
 
-                $(".err").html('');
+        $(".err").html('');
 
-                var userId = sessionStorage.userId;
-                var pageno = 0;
-                var itemPerPage = 10;
-                var projectName = $('#projectName').val();
-                    $('#projectName').focus();
-                    if(projectName.length == 0){
-                        $("#projectNama-error").html("Please Enter the Project Name ");
-                     return false;
-                    }
-                var builder = $('#inputbuilder').val();
-                 $('#inputbuilder').focus();
-                    if(builder.length == 0){
-                        $("#inputbuilder-error").html("Please Enter the Builder Name ");
-                     return false;
-                    }
-                var projecttower= $('#inputprojecttower').val();
-                $('#inputprojecttower').focus();
-                    if(projecttower.length == 0){
-                        $("#inputprojecttower-error").html("Please Enter the Project Tower ");
-                     return false;
-                }
-                var ProjectAddress= $('#inputProjectAddress').val();
-                $('#inputProjectAddress').focus();
-                    if(ProjectAddress.length == 0){
-                        $("#inputProjectAddress-error").html("Please Enter the Project Address ");
-                     return false;
-                }
-                var city = $('#inputProjectCity').val();
-                $('#inputProjectCity').focus();
-                    if(city.length == 0){
-                        $("#inputProjectCity-error").html("Please Enter the City ");
-                     return false;
-                }
-                var state = $('#inputProjectState').val();
-                 $('#inputProjectState').focus();
-                 if(state.length == 0){
-                        $("#inputProjectState-error").html("Please Enter the State ");
-                     return false;
-                 }
-                 var pincode = $('#inputpincode').val();
-                 $('#inputpincode').focus();
-                  if(pincode.length == 0){
-                         $("#inputpincode-error").html("Please Enter the Pin Code ");
-                      return false;
-                  }
-                 var plan = $('#inputplan').val();
-                 $('#inputplan').focus();
-                   if(plan.length == 0){
-                          $("#inputpincode-error").html("Please Enter the Plan ");
-                       return false;
-                 }
+        var userId = sessionStorage.userId;
+        var pageno = 0;
+        var itemPerPage = 10;
+        var projectName = $('#projectName').val();
+        $('#projectName').focus();
+        if(projectName.length == 0){
+        $("#projectNama-error").html("Please Enter the Project Name ");
+        return false;
+        }
+        var builder = $('#inputbuilder').val();
+        $('#inputbuilder').focus();
+        if(builder.length == 0){
+        $("#inputbuilder-error").html("Please Enter the Builder Name ");
+        return false;
+        }
+        var projecttower= $('#inputprojecttower').val();
+        $('#inputprojecttower').focus();
+        if(projecttower.length == 0){
+        $("#inputprojecttower-error").html("Please Enter the Project Tower ");
+        return false;
+        }
+        var ProjectAddress= $('#inputProjectAddress').val();
+        $('#inputProjectAddress').focus();
+        if(ProjectAddress.length == 0){
+        $("#inputProjectAddress-error").html("Please Enter the Project Address ");
+        return false;
+        }
+        var city = $('#inputProjectCity').val();
+        $('#inputProjectCity').focus();
+        if(city.length == 0){
+        $("#inputProjectCity-error").html("Please Enter the City ");
+        return false;
+        }
+        var state = $('#inputProjectState').val();
+        $('#inputProjectState').focus();
+        if(state.length == 0){
+        $("#inputProjectState-error").html("Please Enter the State ");
+        return false;
+        }
+        var pincode = $('#inputpincode').val();
+        $('#inputpincode').focus();
+        if(pincode.length == 0){
+         $("#inputpincode-error").html("Please Enter the Pin Code ");
+        return false;
+        }
+        var plan = $('#inputplan').val();
+        $('#inputplan').focus();
+        if(plan.length == 0){
+          $("#inputpincode-error").html("Please Enter the Plan ");
+        return false;
+        }
 
-                var body={
-                    "builder": $('#inputbuilder').val(),
-                    "city": $('#inputcity').val(),
-                    "description": "",
-                    "id": 0,
-                    "planCode": $('#inputplan').val(),
-                    "projectTower": $('#inputprojecttower').val(),
-                    "projectAddress": $('#inputProjectAddress').val(),
-                    "city": $('#inputProjectCity').val(),
-                    "state": $('#inputProjectState').val(),
-                    "pin": $('#inputpincode').val(),
-                    "projectId": 0,
-                    "projectName": $('#projectName').val(),
-                    "refrencedUserId": 0,
-                    "shareStatus": 0,
-                    "state": "string",
-                    "type": 0,
-                    "userUploadedPlanId": 0
-                }
-                      that.createprojects.createNewProject(userId,{
-                             async: true,
-                             crossDomain: true,
-                             method: "POST",
-                             headers:{
-                                 "authorization": "Bearer "+ sessionStorage.authtoken,
-                                 "Content-Type": "application/json"
-                             },
-                             data: JSON.stringify(body),
-                             success:function(response) {
-                                $('#createProject-modal').modal('hide');
-                                $('#addRoomConcept').modal('show');
+        var formdata={
+        "builder": $('#inputbuilder').val(),
+        "city": $('#inputcity').val(),
+        "description": "",
+        "id": 0,
+        "planCode": $('#inputplan').val(),
+        "projectTower": $('#inputprojecttower').val(),
+        "projectAddress": $('#inputProjectAddress').val(),
+        "city": $('#inputProjectCity').val(),
+        "state": $('#inputProjectState').val(),
+        "pin": $('#inputpincode').val(),
+        "projectId": 0,
+        "projectName": $('#projectName').val(),
+        "refrencedUserId": 0,
+        "shareStatus": 0,
+        "state": "string",
+        "type": 0,
+        "userUploadedPlanId": 0
+        }
+        that.createprojects.createNewProject(userId,{
+             async: true,
+             crossDomain: true,
+             method: "POST",
+             headers:{
+                 "authorization": "Bearer "+ sessionStorage.authtoken,
+                 "Content-Type": "application/json"
+             },
+             data: JSON.stringify(formdata),
+             success:function(response) {
 
-                                 console.log("Successfully create new project- ");
-                                 console.log(body);
+                 console.log("Successfully create new project- ");
+                 console.log(response);
 
-                                 $("#snackbar").html("Successfully create new project...");
-                                var x = document.getElementById("snackbar")
-                                x.className = "show";
-                                setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-                                  that.render();
-                                $(e.currentTarget).closest('article').remove();
-                                 return;
-                             },
-                             error:function(response) {
-                                 console.log(" +++++++++++++++create new project- Errrorr ++++++++++++++++++ ");
-                                 console.log(response);
-                             }
-                      });
+                 $("#snackbar").html("Successfully create new project...");
+                var x = document.getElementById("snackbar")
+                x.className = "show";
+                setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 
-        },
+
+                $('#createProject-modal').modal('hide');
+                $('#addRoomConcept').modal('show');
+
+
+                return false;
+             },
+             error:function(response) {
+                 console.log(" +++++++++++++++create new project- Errrorr ++++++++++++++++++ ");
+                 console.log(response);
+                 return;
+             }
+        });
+        return false;
+
+    },
     getSpaceTypeList: function(){
                 var that = this;
                 return new Promise(function(resolve, reject) {
